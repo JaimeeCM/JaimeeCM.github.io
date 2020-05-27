5b36b80c5780c9f48bb0824c2998f46bcb2bcaf0
Jaime Castel <jaimecst14@gmail.com>
2020-05-27 23:49:11 +0200

    Primer commit

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,21 @@
+/*
+const fila = document.querySelector('.slider');
+const imagenes = document.querySelector('ul');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+flechaDerecha.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth;
+});
+
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+});
+*/
+
+function Mensaje(){
+    alert("Gracias por contactarnos!")
+}
+
+
