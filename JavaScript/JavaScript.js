b29401d76865ec640a15a4960f747e2d0bd2a328
Jaime Castel <49901578+JaimeeCM@users.noreply.github.com>
2020-06-14 14:59:26 +0200

    Mejoras a la página

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,21 +1,76 @@
-/*
-const fila = document.querySelector('.slider');
-const imagenes = document.querySelector('ul');
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
+var imagenes=['../Imagenes/Fitball.jpg','../Imagenes/Horario.jpg','../Imagenes/Contacto.jpg'];
+var cont= 0;
 
-flechaDerecha.addEventListener('click', () => {
-    fila.scrollLeft += fila.offsetWidth;
+function siguiente(slider){
+    slider.addEventListener('click', e => {
+        let atras = slider.querySelector('.flecha-izquierda');
+        let adelante = slider.querySelector('.flecha-derecha');
+        let imagenes = slider.querySelector('#imagenes-slider');
+        let target = e.target;
+
+        if (target == atras){
+            if(cont >0){
+                imagenes.src = imagenes[cont - 1];
+                cont --;
+            }else{
+                imagenes.src=imagenes[imagenes.length - 1];
+                cont = imagenes.length -1 ;
+            }
+        }else if( target == adelante){
+            if(cont < imagenes.length - 1){
+                imagenes.src = imagenes[cont + 1];
+                cont ++;
+            }else{
+                imagenes.src=imagenes[0];
+                cont = 0 ;
+            }
+        }
+    });
+}
+
+document.addEventListener("DOMcontentLoaded", () =>{
+    let slider = document.querySelector('.slider');
+
+    siguiente(slider);
 });
-
-flechaIzquierda.addEventListener('click', () => {
-    fila.scrollLeft -= fila.offsetWidth;
-});
-*/
 
 function Mensaje(){
-    alert("Gracias por contactarnos!")
+    var nombre=document.getElementById('nombre').value;
+    var apellidos=document.getElementById('apellidos').value;
+    var email=document.getElementById('correo').value;
+    var texto=document.getElementById('mensaje').value;
+
+    if (nombre,apellidos,email,texto !=""){
+        alert("Gracias por contactarnos!!");
+    }else{
+        alert("Olvidaste rellenar un campo");
+    }
+}
+
+function mostrarInfo(){
+    var nombre=document.getElementById('nombre').value;
+    var apellidos=document.getElementById('apellidos').value;
+    var email=document.getElementById('correo').value;
+    var texto=document.getElementById('mensaje').value;
+
+    document.getElementById('mostrarInfo').innerHTML="Tus datos introducidos son: "+
+    "Nombre= "+nombre +"//"+
+    "Apellidos= "+apellidos +"//"+
+    "Correo electrónico= "+email +"//"+
+    "Mensaje= "+texto +"//"+
+    "¡¡Gracias por contactar con nosotros!!";
+}
+
+function Login(){
+    var usuario=document.getElementById('usuario').value;
+    var contraseña=document.getElementById('contraseña').value;
+
+    if (usuario,contraseña !=""){
+        alert("Gracias por hacer login!!");
+    }else{
+        alert("Olvidaste rellenar un campo");
+    }
 }
 
 
---
+++ XML/XSL/Tarifas.xsl
@@ -0,0 +1,43 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+  <xsl:template match="tarifas">
+  <html>
+  
+    <head>
+    <link rel="stylesheet" type="text/css" href="../../CSS/EstiloProyecto.css" />
+    </head>
+    
+    <body>
+    
+    <table>
+    <tr>
+      <td colspan="3">Tarifas</td>
+    </tr>
+    
+    <tr>
+      <td>Tipo</td>
+      <td>Duracion</td>
+      <td>Informacion</td>
+      <td>Activacion</td>
+      <td>Precio</td>
+    </tr>
+    
+    
+    <xsl:apply-templates select="tarifa"/>
+    </table>
+    </body>
+    
+  </html>
+  </xsl:template>
+
+<xsl:template match="tarifa">
+    <tr>
+      <td><xsl:value-of select="tipo"/></td>
+      <td><xsl:value-of select="duracion"/></td>
+      <td><xsl:value-of select="informacion"/></td>
+      <td><xsl:value-of select="activacion"/></td>
+      <td><xsl:value-of select="precio"/></td>
+    </tr>
+</xsl:template>
+  
+</xsl:stylesheet>
